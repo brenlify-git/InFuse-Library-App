e500ccdd4d1fa0709db297e44516d918014f4a14
Brenlify <robles.167967@gmail.com>
2023-01-08 00:25:07 +0800

    with tables andd form inputs

--- sidebar.php
+++ sidebar.php
@@ -61,41 +61,19 @@
                 </a>
                 <ul id="tables-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
                     <li>
-                        <a href="tables-general.html">
-                            <i class="bi bi-circle"></i><span>General Tables</span>
+                        <a href="add-books.php">
+                            <i class="bi bi-circle"></i><span>Add Book</span>
                         </a>
                     </li>
                     <li>
-                        <a href="tables-data.html">
-                            <i class="bi bi-circle"></i><span>Data Tables</span>
+                        <a href="update-books.php">
+                            <i class="bi bi-circle"></i><span>Update Book</span>
                         </a>
                     </li>
                 </ul>
             </li><!-- End Tables Nav -->
 
-            <li class="nav-item">
-                <a class="nav-link collapsed" data-bs-target="#charts-nav" data-bs-toggle="collapse" href="#">
-                    <i class="bi bi-journal-plus"></i><span>Book Acquire</span><i
-                        class="bi bi-chevron-down ms-auto"></i>
-                </a>
-                <ul id="charts-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-                    <li>
-                        <a href="charts-chartjs.html">
-                            <i class="bi bi-circle"></i><span>Chart.js</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="charts-apexcharts.html">
-                            <i class="bi bi-circle"></i><span>ApexCharts</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="charts-echarts.html">
-                            <i class="bi bi-circle"></i><span>ECharts</span>
-                        </a>
-                    </li>
-                </ul>
-            </li><!-- End Charts Nav -->
+           
             <li class="nav-item">
                 <a class="nav-link collapsed" href="users-profile.html">
                     <i class="bi bi-journal-minus"></i>
@@ -111,19 +89,15 @@
                 <ul id="icons-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
                     <li>
                         <a href="icons-bootstrap.html">
-                            <i class="bi bi-circle"></i><span>Bootstrap Icons</span>
+                            <i class="bi bi-circle"></i><span>Book Borrow</span>
                         </a>
                     </li>
                     <li>
                         <a href="icons-remix.html">
-                            <i class="bi bi-circle"></i><span>Remix Icons</span>
+                            <i class="bi bi-circle"></i><span>Book Return</span>
                         </a>
                     </li>
-                    <li>
-                        <a href="icons-boxicons.html">
-                            <i class="bi bi-circle"></i><span>Boxicons</span>
-                        </a>
-                    </li>
+                 
                 </ul>
             </li><!-- End Icons Nav -->
 
@@ -154,15 +128,7 @@
                         <a href="book-return.php">
                             <i class="bi bi-circle"></i><span>Book Returned</span>
                         </a>
-                        <a href="">
-                            <i class="bi bi-circle"></i><span>Book Overdue</span>
-                        </a>
-                        <a href="">
-                            <i class="bi bi-circle"></i><span>With Penalties</span>
-                        </a>
-                        <a href="">
-                            <i class="bi bi-circle"></i><span>Payment Transactions</span>
-                        </a>
+                        
                         
                     </li>
 
---
+++ update-books.php
@@ -0,0 +1,212 @@
+<?php 
+
+include 'connection.php';
+
+
+$sql = "SELECT * FROM tbl_bookinfo";
+
+$id = $conn->query($sql);
+
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>InFuse | Patron Masterlist</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.gstatic.com" rel="preconnect">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+  <link rel="stylesheet" href="assets/css/table.css">
+
+</head>
+
+<body>
+
+  <!-- ======= Sidebar and Header ======= -->
+
+  <?php include 'headerbar.php';?>
+  <?php include 'sidebar.php';?>
+
+  <!-- End Sidebar and Header-->
+
+  
+
+  <main id="main" class="main">
+  <div class="leftColumn col-7">
+
+    <div class="pagetitle">
+      <h1>Update Book</h1>
+      <nav>
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
+          <li class="breadcrumb-item">Book Entry</li>
+          <li class="breadcrumb-item active">Add Book</li>
+        </ol>
+      </nav>
+    </div><!-- End Page Title -->
+
+   <section class="section">
+      <div class="row">
+        <div class="col-lg-12">
+
+
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Insert Book Details</h5>
+
+              <!-- Multi Columns Form -->
+              <form class="row g-3">
+                <div class="col-md-4">
+                  <label for="inputName5" class="form-label">Customize ID</label>
+                  <input type="number" class="form-control" id="inputName5" value ="1" required>
+                </div>
+                <div class="col-md-4">
+                  <label for="inputName5" class="form-label">Barcode ID</label>
+                  <input type="number" class="form-control" id="inputName5" required>
+                </div>
+                <div class="col-md-4">
+                  <label for="inputEmail5" class="form-label">Call Number</label>
+                  <input type="number" class="form-control" id="inputEmail5" required>
+                </div>
+                <div class="col-md-12">
+                  <label for="inputPassword5" class="form-label">Book Name</label>
+                  <input type="text" class="form-control" id="inputPassword5" required>
+                </div>
+                <div class="col-12">
+                  <label for="inputAddress5" class="form-label">Book Author(s)</label>
+                  <input type="text" class="form-control" id="inputAddres5s" placeholder="William Shakespeare" required>
+                </div>
+                
+                <div class="col-6">
+                    <label for="inputDate" class="form-label">Year Published</label>
+                    <div class="col-sm-12">
+                        <input type="date" class="form-control" required>
+                    </div>
+                </div>
+                <div class="col-6">
+                  <label for="inputAddress5" class="form-label">ISBN</label>
+                  <input type="number" class="form-control" id="inputAddres5s" placeholder="William Shakespeare" required>
+                </div>
+                <div class="col-4">
+                  <label class="col-sm-2 form-label">Subject</label>
+                  <div class="col-sm-12">
+                    <select class="form-select" aria-label="Default select example" required>
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">Three</option>
+                    </select>
+                  </div>
+                </div>
+                <div class="col-4">
+                  <label class="col-sm-3 form-label">Category</label>
+                  <div class="col-sm-12">
+                    <select class="form-select" aria-label="Default select example">
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">Three</option>
+                    </select>
+                  </div>
+                </div>
+                <div class="col-4">
+                  <label for="inputAddress5" class="form-label">Series</label>
+                  <input type="number" class="form-control" id="inputAddres5s" placeholder="2014" required>
+                </div>
+                <div class="col-4">
+                  <label for="inputAddress5" class="form-label">Price</label>
+                  <input type="number" class="form-control" id="inputAddres5s" placeholder="567.00" required>
+                </div>
+                <div class="col-4">
+                  <label for="inputAddress5" class="form-label">Copies</label>
+                  <input type="number" class="form-control" id="inputAddres5s" placeholder="5" required>
+                </div>
+                <div class="col-4">
+                  <label for="inputAddress5" class="form-label">Publisher</label>
+                  <input type="text" class="form-control" id="inputAddres5s" placeholder="Book Shelf Inc." required>
+                </div>
+                <div class="col-6">
+                  <label class="col-sm-3 form-label">Status</label>
+                  <div class="col-sm-12">
+                    <select class="form-select" aria-label="Default select example">
+                      <option value="1">Available</option>
+                      <option value="2">Unavailable</option>
+                    </select>
+                  </div>
+                </div>
+                <div class="col-6">
+                  <label for="inputAddress5" class="form-label">Notes</label>
+                  <textarea type="number" class="form-control" id="inputAddres5s" placeholder="Cover page has dirt marks." required></textarea>
+                </div>
+                
+               
+                <div class="text-right">
+                  <button type="submit" class="btn btn-primary ">Submit</button>
+                  <button type="reset" class="btn btn-warning">Reset</button>
+                </div>
+              </form><!-- End Multi Columns Form -->
+
+            </div>
+          </div>
+
+        </div>
+
+
+      </div>
+    </section>
+
+  </div>
+
+  </main><!-- End #main -->
+
+
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer" class="footer">
+    
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/chart.js/chart.umd.js"></script>
+  <script src="assets/vendor/echarts/echarts.min.js"></script>
+  <script src="assets/vendor/quill/quill.min.js"></script>
+  <script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
+  <script src="assets/vendor/tinymce/tinymce.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
