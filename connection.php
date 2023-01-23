f35547d03a21ee9a5896def93eeeabbb9f366e7b
Brenlify <robles.167967@gmail.com>
2023-01-24 03:06:42 +0800

    send to hostinger

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 
 $servername = "localhost";
-$username = "root";
-$password = "";
-$database = "infuse_library_app";
+$username = "u666422188_brenlify";
+$password = "Yehey_01";
+$database = "u666422188_infuselibrary";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
@@ -15,6 +15,20 @@ if ($conn->connect_error) {
 
 // echo "Connected";
 ?>
+
+
+<!-- $servername = "localhost";
+$username = "root";
+$password = "";
+$database = "infuse_library_app";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} -->
 
 
 
